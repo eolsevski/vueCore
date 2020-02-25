68e04daf9de3921ce9efef7ec233596edf6d0722
Edvard Olševski <edvard.olsevski@gjensidige.lt>
2020-02-25 15:31:28 +0200

    simple loging midway

---
+++ bundeled/clientapp/src/components/Login.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <input type="text" placeholder="username" v-model="userName">
+        <input type="password" placeholder="password" v-model="password">
+        <input v-model="token">
+        <button @click="Login">Login</button>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+import axios from 'axios';
+
+export default Vue.extend({
+data(){
+    return{
+        userName:'',
+        password:'',
+        token:'undefined'
+    }
+    },
+    methods: {
+        Login: function(){
+            const options = {
+                method: 'post',
+                url:'/weatherforecast/login',
+                data: {
+                    username: this.userName,
+                    password: this.password
+                }
+            }
+            /* eslint-disable no-console */
+            axios(options).then((response) => this.token = response.data);
+            console.log(this.token);
+            /* eslint-enable no-console */
+        }
+    }
+
+   
+})
+</script>
---
+++ bundeled/clientapp/src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <login/>
+    </div>
+</template>
+
+<script>
+import Login from '@/components/Login.vue'
+
+export default {
+components:{
+    Login
+}    
+}
+</script>
