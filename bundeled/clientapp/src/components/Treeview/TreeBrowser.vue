bbe50344f95eb5ca302c3454c1520fe84206573a
Edvard Olševski <edvard.olsevski@gjensidige.lt>
2020-06-17 19:09:07 +0300

    tree view component with check box, working

---
+++ bundeled/clientapp/src/components/Treeview/TreeBrowser.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    
+    <div :style="{'margin-left': `${depth * 35}px`}" class="node">
+      <i
+        @click="expandCollapsed"
+        v-if="hasChildren"
+        class="fa fa-folder-o"
+        v-bind:class="{ 'fa fa-folder-open-o': expanded }"
+      ></i>
+      {{node.name}}
+      <input
+        type="checkbox"
+        id="jack"
+        v-bind:value="node.id"
+        v-model="checkd"
+        @click="inputChecked(node.fulPath, checkd)"
+      />
+    
+    </div>
+
+    <!-- eslint-disable -->
+    <TreeBrowser
+      v-if="expanded"
+      v-for="child in node.children"
+      :key="child.id"
+      :node="child"
+      :depth="depth + 1"
+      @inputClicked="inputChecked"
+    />
+    
+    <!-- eslint-enable -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TreeBrowser",
+  props: {
+    node: Object,
+    depth: {
+      type: Number,
+      default: 0
+    },
+    name: {
+      type: String,
+      default: ""
+    },
+    checked:{
+      type: Boolean,
+      default: false
+    }
+    
+  },
+
+  data() {
+    return {
+      checkd: false,
+      expanded: false
+    };
+  },
+  methods: {
+    expandCollapsed() {
+      this.expanded = !this.expanded;
+      //let newName = ""+ this.node.name + name;
+      if (!this.hasChildren) {
+        //this.$emit("onClick", node, newName);
+      }
+    },
+    inputChecked(name, checked){
+      //alert('midway catch: ' + " name: " + name);
+      this.$emit('inputClicked', name, checked);
+    }
+  },
+  computed: {
+    hasChildren() {
+      return this.node.children;
+    }
+  }
+};
+</script>
+
+<style >
+.node {
+  text-align: left;
+}
+</style>
--- bundeled/clientapp/src/components/Upload/uploadView.vue
+++ bundeled/clientapp/src/components/Upload/uploadView.vue
@@ -1,15 +1,70 @@
 <template>
   <div class="upload">
     <upload/>
+    <treeBrowser
+    :node="root"
+    @onClick="nodeWasClicked"
+    @inputClicked="markItem"
+    />
+    <treeBrowser2  :tree-data="root" @node-click="markItem"/>
+    <span>Checked names: {{checkedItems}}</span>
   </div>
 </template>
 
-
 <script>
 import upload from '@/components/Upload/upload'
+import treeBrowser from '../Treeview/TreeBrowser'
+
+
 export default {
+  data() {
+    return {
+      checkedItems: [],
+      root: {
+        name: 'My Own Files',
+        fulPath: 'My Own Files',
+        children: [
+          {
+            name: 'music',
+            fulPath: 'My Own Files/music',
+            children:[
+              {
+                name:'song.mp3',
+                fulPath: 'My Own Files/music/song.mp3',
+              }
+            ]
+          },
+          {
+            fulPath: 'My Own Files/workspace',
+            name:'workspace',
+            children:[
+              {
+                fulPath: 'My Own Files/workspace/source.smthng',
+                name: 'source.smthng',
+                
+              }
+            ]
+          }
+        ]
+      }
+    }
+  },
+  methods:{
+    nodeWasClicked(node){
+alert(node.id);
+    },
+    markItem(name, checked){
+      if(checked)
+      {this.checkedItems.splice(this.checkedItems.indexOf(name),1)}
+      else{
+        this.checkedItems.push(name);
+      }
+      
+    }
+  },
     components:{
-        upload
+        upload,
+        treeBrowser
     }
 }
 </script>
